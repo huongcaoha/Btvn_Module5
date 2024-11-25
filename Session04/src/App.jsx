df88d2508d20cc232e98534871d4fc29665807f0
huongcaoha <huongcaoha1994@gmail.com>
2024-11-25 17:12:02 +0700

    update btvn session 04

---
+++ Session04/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import OnwayAndTwowayDataBinding from "./components/OnwayAndTwowayDataBinding";
+import DemoUseEffect from "./components/DemoUseEffect";
+import DemoUseRef from "./components/DemoUseRef";
+import Baitap11 from "./Btvn/baitap11/Baitap11";
+import Bai5 from "./Btvn/Bai5";
+import Bai7 from "./Btvn/Bai7";
+import Bai8 from "./Btvn/Bai8";
+import Bai9 from "./Btvn/Bai9";
+import Bai10 from "./Btvn/Bai10";
+
+function App() {
+  return (
+    <>
+      <Baitap11 />
+      <Bai5 />
+      <Bai7 />
+      <Bai8 />
+      <Bai9 />
+      <Bai10 />
+    </>
+  );
+}
+
+export default App;
---
+++ Session04/src/Btvn/baitap12/Bai12.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Bai12() {
+  return <div>Bai12</div>;
+}
